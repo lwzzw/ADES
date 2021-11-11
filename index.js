b4fb71a8657a46facef271282d059ba5a3e6b248
Tan Jun Heng <junheng.20@ichat.sp.edu.sg>
2021-11-11 09:46:25 +0800

    removed module

--- index.js
+++ index.js
@@ -19,22 +19,22 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/html')));
 
-// app.use(ApiRouter);
+app.use(ApiRouter);
 
-// // 404 Handler
-// app.use((req, res, next) => {
-//     console.log('404');
-//     next(createHttpErrors(404, `Unknown Resource ${req.method} ${req.originalUrl}`));
-// });
+// 404 Handler
+app.use((req, res, next) => {
+    console.log('404');
+    next(createHttpErrors(404, `Unknown Resource ${req.method} ${req.originalUrl}`));
+});
 
-// // Error Handler
-// app.use((error, req, res, next) => {
-//     console.error(error);
-//     return res.status(error.status || 500).json({
-//         error: error.message || `Unknown Error!`,
-//         status: error.status
-//     });
-// });
+// Error Handler
+app.use((error, req, res, next) => {
+    console.error(error);
+    return res.status(error.status || 500).json({
+        error: error.message || `Unknown Error!`,
+        status: error.status
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`App listen on port http://localhost:${port}`);
