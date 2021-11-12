6e0c60a055f0c26e21f0b13c9aeda17d59197a8e
Tan Jun Heng <junheng.20@ichat.sp.edu.sg>
2021-11-12 20:48:13 +0800

    implemented categories in dropdown menu

---
+++ public/js/index-main.js
@@ -0,0 +1,32 @@
+function getAllCategories() {
+    const methods = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    return axios
+        .get(`/game/getAllCategories`, methods)
+        .then(response => {
+            return response.data.categories
+        }).catch(error => {
+            console.log(error);
+            if (error.response) {
+                throw new Error(JSON.stringify(error.response.data))
+            }
+            return error.response.data
+        })
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    const dropDownButton = document.getElementById('dropDownCategory');
+    
+    getAllCategories().then(response => {
+        for (let i=0; i < response.length; i++) {
+            const category = response[i];
+            const postHtml = `<a class="dropdown-item" '>${category.category_name}</a>`
+            dropDownButton.insertAdjacentHTML('beforeend', postHtml)
+        }
+    })
+
+})
--- router/gameRouter.js
+++ router/gameRouter.js
@@ -4,4 +4,12 @@ const createHttpError = require('http-errors');
 const router = require('express').Router();
 
 
+router.get('/getAllCategories', function (req, res, next) {
+    return database
+        .query(`SELECT category_name FROM g2a_category`)
+        .then(result => {
+            return res.status(200).json({categories: result.rows})
+        })
+})
+
 module.exports = router;
