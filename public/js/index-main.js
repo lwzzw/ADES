425da81d01c2067ad35bb76b42cbb32a405bd84a
Tan Jun Heng <junheng.20@ichat.sp.edu.sg>
2021-11-13 14:11:35 +0800

    fixed dropdown bar not changing

--- public/js/index-main.js
+++ public/js/index-main.js
@@ -20,11 +20,11 @@ function getAllCategories() {
 
 window.addEventListener('DOMContentLoaded', function () {
     const dropDownButton = document.getElementById('dropDownCategory');
-    
+
     getAllCategories().then(response => {
         for (let i=0; i < response.length; i++) {
             const category = response[i];
-            const postHtml = `<a class="dropdown-item" '>${category.category_name}</a>`
+            const postHtml = `<option value='${category.category_name}' '>${category.category_name}</option>`
             dropDownButton.insertAdjacentHTML('beforeend', postHtml)
         }
     })
