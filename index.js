5899c3c5239262521941460e6785b82e32054be9
Tan Jun Heng <junheng.20@ichat.sp.edu.sg>
2021-11-15 09:36:42 +0800

    added database scripts

--- index.js
+++ index.js
@@ -12,31 +12,59 @@ db.connect()
         if (response) {
             return db.query(
                 `
-        CREATE TABLE IF NOT EXISTS G2A_gameDatabase (
-          game_id SERIAL primary key,
-          game_name VARCHAR (50) not null,
-          game_platform VARCHAR (50) not null,
-          game_price NUMERIC(12,2) not null,
-          game_description VARCHAR(255) not null,
-          game_image VARCHAR (255) not null
+
+        
+        CREATE TABLE IF NOT EXISTS main_category (
+          id SERIAL primary key,
+          category_name VARCHAR (50) not null,
         );
 
-        CREATE TABLE IF NOT EXISTS G2A_category (
-          category_name VARCHAR (50) not null,
-          fk_game_id INT not null
+        CREATE TABLE IF NOT EXISTS parent_subcategory (
+          id SERIAL primary key,
+          sub_category_name VARCHAR (50) not null,
+          fk_main INT not null,
+
         );
+
+        CREATE TABLE IF NOT EXISTS child_subcategory (
+            id SERIAL primary key,
+            child_category_name VARCHAR (50) not null,
+            fk_parent INT not null,
+          );
+
+          DROP TABLE G2A_gameDatabase;
+          CREATE TABLE IF NOT EXISTS G2A_gameDatabase (
+            gid SERIAL primary key,
+            g_name VARCHAR (50) not null,
+            g_platform VARCHAR (50) not null,
+            g_price NUMERIC(12,2) not null,
+            g_description VARCHAR null,
+            g_maincategory INT not null,
+            g_parentSubcategory INT null,
+            g_childSubcategory INT null,
+            g_image VARCHAR (255) null,
+            g_publishDate DATE not null,
+            CONSTRAINT fk_main FOREIGN KEY(g_maincategory) REFERENCES main_category(id),
+            CONSTRAINT fk_parent FOREIGN KEY(g_parentSubcategory) REFERENCES parent_subcategory(id),
+            CONSTRAINT fk_child FOREIGN KEY(g_childSubcategory) REFERENCES child_subcategory(id)
+          );
+          
+        CREATE TABLE IF NOT EXISTS order_history (
+            historyid SERIAL primary key,
+
+        )
         `
             )
-                .then(response => {
-                    return db.query(`
-                INSERT INTO g2a_category (category_name, fk_game_id) SELECT 'MOBA', 1 WHERE NOT EXISTS (SELECT * FROM g2a_category WHERE category_name = 'MOBA');
-                INSERT INTO g2a_category (category_name, fk_game_id) SELECT 'MMO', 1 WHERE NOT EXISTS (SELECT * FROM g2a_category WHERE category_name = 'MMO');
-                INSERT INTO g2a_gamedatabase (game_name, game_platform, game_price,game_description, game_image) SELECT 'DOTA 2', 'PC', 99.9, 'EZ GAME', 'image' WHERE NOT EXISTS (SELECT * FROM g2a_gamedatabase WHERE game_name = 'DOTA 2');
-                 `)
-                })
-                .then(response => {
-                    return db.query(`SELECT g2a_category.fk_game_id, g2a_gamedatabase.game_name, string_agg(g2a_category.category_name, ',') FROM g2a_category INNER JOIN g2a_gamedatabase ON g2a_category.fk_game_id = g2a_gamedatabase.game_id GROUP BY g2a_category.fk_game_id,g2a_gamedatabase.game_name`);
-                })
+                // .then(response => {
+                //     return db.query(`
+                // INSERT INTO main_category (category_name, fk_game_id) SELECT 'MOBA', 1 WHERE NOT EXISTS (SELECT * FROM main_category WHERE category_name = 'MOBA');
+                // INSERT INTO main_category (category_name, fk_game_id) SELECT 'MMO', 1 WHERE NOT EXISTS (SELECT * FROM main_category WHERE category_name = 'MMO');
+                // INSERT INTO g2a_gamedatabase (g_name, g_platform, g_price, g_description, g_image, g_publishdate) SELECT 'DOTA 2', 'PC', 99.9, 'EZ GAME', 'image', CURRENT_TIMESTAMP WHERE NOT EXISTS (SELECT * FROM g2a_gamedatabase WHERE g_name = 'DOTA 2');
+                //  `)
+                // })
+                // .then(response => {
+                //     return db.query(`SELECT main_category.fk_game_id, g2a_gamedatabase.g_name, string_agg(main_category.category_name, ',') FROM main_category INNER JOIN g2a_gamedatabase ON main_category.fk_game_id = g2a_gamedatabase.gid GROUP BY main_category.fk_game_id,g2a_gamedatabase.g_name`);
+                // })
                 .then(response => {
                     console.log(response.rows)
 
--- public/js/index-main.js
+++ public/js/index-main.js
@@ -2,15 +2,15 @@ window.addEventListener('DOMContentLoaded', function () {
     const dropDownButton = document.getElementById('dropDownCategory');
     // const headerCategory = document.getElementById('header-categories')
 
-    getAllCategories().then(response => {
-        for (let i = 0; i < response.length; i++) {
-            const category = response[i];
-            const postHtml = `<option value='${category.category_name}' '>${category.category_name}</option>`
-            dropDownButton.insertAdjacentHTML('beforeend', postHtml)
-            // const postHeaderCategory = `<li class='navbar-category'><a href="#">${category.category_name}</a></li>`
-            // headerCategory.insertAdjacentHTML('beforeend', postHeaderCategory)
-        }
-    })
+    // getAllCategories().then(response => {
+    //     for (let i = 0; i < response.length; i++) {
+    //         const category = response[i];
+    //         const postHtml = `<option value='${category.category_name}' '>${category.category_name}</option>`
+    //         dropDownButton.insertAdjacentHTML('beforeend', postHtml)
+    //         // const postHeaderCategory = `<li class='navbar-category'><a href="#">${category.category_name}</a></li>`
+    //         // headerCategory.insertAdjacentHTML('beforeend', postHeaderCategory)
+    //     }
+    // })
 
 })
 
