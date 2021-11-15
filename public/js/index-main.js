d5271335f478b89d3d6e00a2a84feae42585dc18
me <weixin1914@gmail.com>
2021-11-15 15:19:42 +0800

    yeah

--- public/js/index-main.js
+++ public/js/index-main.js
@@ -2,93 +2,111 @@ window.addEventListener('DOMContentLoaded', function () {
     const dropDownButton = document.getElementById('dropDownCategory');
     // const headerCategory = document.getElementById('header-categories')
 
-    // getAllCategories().then(response => {
-    //     for (let i = 0; i < response.length; i++) {
-    //         const category = response[i];
-    //         const postHtml = `<option value='${category.category_name}' '>${category.category_name}</option>`
-    //         dropDownButton.insertAdjacentHTML('beforeend', postHtml)
-    //         // const postHeaderCategory = `<li class='navbar-category'><a href="#">${category.category_name}</a></li>`
-    //         // headerCategory.insertAdjacentHTML('beforeend', postHeaderCategory)
-    //     }
-    // })
-
-})
-
-var list = document.getElementsByClassName("cat-content");
-var first_cat = [],
-    second_cat = [],
-    third_cat = [];
-
-Array.from(list).forEach(e => {
-    if (e.classList.contains("first-cat")) {
-        first_cat.push(e.id);
-    } else if (e.classList.contains("second-cat")) {
-        second_cat.push(e.id);
-    } else {
-        third_cat.push(e.id);
-    }
-})
-Array.from(list).forEach(e => {
-    if (e.classList.contains("first-cat")) {
-        e.addEventListener("mouseover", function () {
-            first_cat.forEach(id => {
-                try {
-                    document.getElementById(`1-${id}`).style.display = "none";
-                } catch (e) {}
-            });
-            second_cat.forEach(id => {
-                try {
-                    document.getElementById(`2-${id}`).style.display = "none";
-                } catch (e) {}
-            });
-            try {
-                document.getElementById("bg").style.display = "block";
-                setTimeout(function(){
-                    document.getElementById("bg").style.opacity = "0.8";
-                }, 0);
-                Array.from(document.getElementsByClassName("img-second")).forEach(e=>{
-                    e.style.display="block";
-                })
-                document.getElementById(`1-${e.id}`).style.display = "block";
-                Array.from(document.getElementsByClassName("img-second")).forEach(e=>{
-                    e.style.display="none";
-                })
-                Array.from(document.getElementsByClassName("img-third")).forEach(e=>{
-                    e.style.display="block";
-                })
-            } catch (e) {}
-        })
-    }
-    if (e.classList.contains("second-cat")) {
-        e.addEventListener("mouseover", function () {
-            second_cat.forEach(id => {
-                try {
-                    document.getElementById(`2-${id}`).style.display = "none";
-                } catch (e) {}
-            });
-            try {
-                document.getElementById(`2-${e.id}`).style.display = "block";
-                Array.from(document.getElementsByClassName("img-third")).forEach(e=>{
-                    e.style.display="none";
-                })
-            } catch (e) {}
-        })
-    }
-})
-document.getElementById("bg").addEventListener("mouseover",function(){
-    document.getElementById("bg").style.opacity = "0";
-    document.getElementById("bg").style.display = "none";
-    first_cat.forEach(id => {
-        try {
-            document.getElementById(`1-${id}`).style.display = "none";
-        } catch (e) {}
-    });
-    second_cat.forEach(id => {
-        try {
-            document.getElementById(`2-${id}`).style.display = "none";
-        } catch (e) {}
-    });
-    Array.from(document.getElementsByClassName("img-default")).forEach(e=>{
-        e.style.display="block";
+    getAllCategories().then(response => {
+        console.log(response)
+        for (let i = 0; i < response.length; i++) {
+            let string = `<li class="cat-content first-cat" id="${response[i].id}"><a href="/filter?main_cat=${encodeURI(response[i].category_name)}">${response[i].category_name}</a></li>`;
+            document.getElementById("first_cat").insertAdjacentHTML("beforeend", string);
+            if (response[i].parent.length == 0) break;
+            string = `<div class="cat-block" id="1-${response[i].id}"><ul>`;
+            for (let j = 0; j < response[i].parent.length; j++) {
+                console.log(response[i].parent[j].id)
+                string += `<li class="cat-content second-cat" id="1-${response[i].parent[j].fk_main}-${response[i].parent[j].id}"><a href="/filter?main_cat=${encodeURI(response[i].parent[j].category_name)}">${response[i].parent[j].category_name}</a></li>`;
+                if (response[i].parent[j].child.length == 0) break;
+                let thirdstring = `<div class="cat-content third-cat cat-block" id="2-${response[i].parent[j].fk_main}-${response[i].parent[j].id}"><ul>`;
+                for (let k = 0; k < response[i].parent[j].child.length; k++) {
+                    thirdstring += `<li>${response[i].parent[j].child[k].category_name}</li>`;
+                }
+                thirdstring += `</ul></div>`;
+                document.getElementById("third_cat").insertAdjacentHTML("beforeend", thirdstring);
+            }
+            string += `</ul></div>`;
+            document.getElementById("second_cat").insertAdjacentHTML("beforeend", string);
+        }
+        addListener();
     })
 })
+
+function addListener() {
+    var list = document.getElementsByClassName("cat-content");
+    var first_cat = [],
+        second_cat = [],
+        third_cat = [];
+
+    Array.from(list).forEach(e => {
+        if (e.classList.contains("first-cat")) {
+            first_cat.push(e.id);
+        } else if (e.classList.contains("second-cat")) {
+            second_cat.push(e.id);
+        } else {
+            third_cat.push(e.id);
+        }
+    })
+    Array.from(list).forEach(e => {
+        if (e.classList.contains("first-cat")) {
+            e.addEventListener("mouseover", function () {
+                first_cat.forEach(id => {
+                    try {
+                        document.getElementById(`1-${id}`).style.display = "none";
+                    } catch (e) {}
+                });
+                second_cat.forEach(id => {
+                    try {
+                        document.getElementById(`2-${id}`).style.display = "none";
+                    } catch (e) {}
+                });
+                try {
+                    document.getElementById("bg").style.display = "block";
+                    setTimeout(function () {
+                        document.getElementById("bg").style.opacity = "0.8";
+                    }, 0);
+                    Array.from(document.getElementsByClassName("img-second")).forEach(e => {
+                        e.style.display = "block";
+                    })
+                    document.getElementById(`1-${e.id}`).style.display = "block";
+                    Array.from(document.getElementsByClassName("img-second")).forEach(e => {
+                        e.style.display = "none";
+                    })
+                    Array.from(document.getElementsByClassName("img-third")).forEach(e => {
+                        e.style.display = "block";
+                    })
+                } catch (e) {}
+            })
+        }
+        if (e.classList.contains("second-cat")) {
+            e.addEventListener("mouseover", function () {
+                second_cat.forEach(id => {
+                    try {
+                        document.getElementById(`2-${id.substr(2)}`).style.display = "none";
+                    } catch (e) {}
+                });
+                try {
+                    document.getElementById(`2-${e.id.substr(2)}`).style.display = "block";
+                    Array.from(document.getElementsByClassName("img-third")).forEach(e => {
+                        e.style.display = "none";
+                    })
+                } catch (e) {}
+            })
+        }
+    })
+    document.getElementById("bg").addEventListener("mouseover", function () {
+        document.getElementById("bg").style.opacity = "0";
+        document.getElementById("bg").style.display = "none";
+        first_cat.forEach(id => {
+            try {
+                document.getElementById(`1-${id}`).style.display = "none";
+            } catch (e) {}
+        });
+        second_cat.forEach(id => {
+            try {
+                document.getElementById(`2-${id}`).style.display = "none";
+            } catch (e) {}
+        });
+        Array.from(document.getElementsByClassName("img-default")).forEach(e => {
+            e.style.display = "block";
+        })
+    })
+    console.log(first_cat)
+    console.log(second_cat)
+    console.log(third_cat)
+}
--- router/gameRouter.js
+++ router/gameRouter.js
@@ -9,15 +9,16 @@ router.get('/getAllCategories', async function (req, res, next) {
         let dbResult = await database.query("SELECT id, category_name FROM main_category").then(result => result).catch(err => {
             next(createHttpError(500, err))
         });
+        dbResult.rows.main = dbResult.rows;
         for (let i = 0; i < dbResult.rows.length; i++) {
-            let parentcat = await database.query(`SELECT id, category_name FROM parent_subcategory where fk_main=$1;`, [dbResult.rows[i].id])
+            let parentcat = await database.query(`SELECT id, category_name, fk_main FROM parent_subcategory where fk_main=$1;`, [dbResult.rows[i].id])
                 .then(result => result)
                 .catch(err => {
                     next(createHttpError(500, err))
                 });
             dbResult.rows[i].parent = parentcat.rows;
             for (let j = 0; j < parentcat.rows.length; j++) {
-                let childcat = await database.query(`SELECT id, category_name FROM child_subcategory where fk_parent=$1;`, [parentcat.rows[j].id])
+                let childcat = await database.query(`SELECT id, category_name, fk_parent FROM child_subcategory where fk_parent=$1;`, [parentcat.rows[j].id])
                     .then(result => result)
                     .catch(err => {
                         next(createHttpError(500, err))
