c147cb65436eb64c3fad4ea430adacaeedb31de5
me <weixin1914@gmail.com>
2021-11-16 13:44:12 +0800

    category show/close

---
+++ public/js/category-api.js
@@ -0,0 +1,20 @@
+function getAllCategories() {
+    const methods = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    return axios
+        .get(`/game/getAllCategories`, methods)
+        .then(response => {
+            return response.data.categories
+        })
+        .catch(error => {
+            console.log(error);
+            if (error.response) {
+                throw new Error(JSON.stringify(error.response.data))
+            }
+            return error.response.data
+        })
+}
