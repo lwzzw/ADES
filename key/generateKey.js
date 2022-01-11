b6410b61c675989b4619cb1250256a63a2be288f
me <weixin1914@gmail.com>
2022-01-11 19:13:57 +0800

    send key via email if user didt login, combine insert query to reduce waiting time

---
+++ key/generateKey.js
@@ -0,0 +1,9 @@
+module.exports = (length) => {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var charactersLength = characters.length;
+  let string = "";
+  for (var i = 0; i <= length; i++) {
+    string += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return string;
+};
--- router/orderRouter.js
+++ router/orderRouter.js
@@ -7,7 +7,8 @@ const verifyToken = require("../middleware/checkUserAuthorize");
 const axios = require("axios");
 const nodeCache = require("node-cache");
 const config = require("../config");
-const sendMail = require("../email/email").sendMail
+const sendMail = require("../email/email").sendMail;
+const generateKey = require("../key/generateKey");
 const Environment =
   config.ENV === "production"
     ? paypal.core.LiveEnvironment
@@ -108,7 +109,6 @@ router.post("/save-order", async (req, res, next) => {
         },
       })
       .then((res) => res.data);
-      console.log(paypalRes);
     if (paypalRes.status !== "COMPLETED" || paypalRes.id != cache.get(id)) {
       return next(createHttpError(400, "transaction not complete"));
     }
@@ -124,19 +124,44 @@ router.post("/save-order", async (req, res, next) => {
       id,
       total,
     ]);
-
-    await database.transactionQuery(`select order_id, g_id, amount FROM order_detail WHERE order_id = $1`, [hid.rows[0].confirm_order]).then(result => {
-      let orderList = result.rows
-      orderList.forEach(orderDetail => {
-        let amount = orderDetail.amount
-        
-        for (let i = 0; i < amount; i++) {
-          database.query(`INSERT INTO keys (order_id, g_id, key) VALUES($1, $2, $3)`, [orderDetail.order_id, orderDetail.g_id, generateKey()])
-        }
-      })
-    })
-    let text = `You have been successful make a payment on ${paypalRes.create_time}`
-    sendMail(paypalRes.payer.email_address,"Thank you for purchase", {text});
+    let emailHtml
+    await database
+      .transactionQuery(
+        `select order_id, order_detail.g_id, amount, g_name FROM order_detail join G2A_gameDatabase on order_detail.g_id = G2A_gameDatabase.g_id  WHERE order_id = $1`,
+        [hid.rows[0].confirm_order]
+      )
+      .then(async (result) => {
+        let isUser = await database
+          .query(`select 1 from user_detail where id = $1`, [id])
+          .then((result) => result.rows.length == 1);
+        let orderList = result.rows;
+        orderList.forEach((orderDetail) => {
+          console.log(isUser);
+          if (isUser) {
+            insertKey(orderDetail.amount);
+            function insertKey(amount) {
+              let string = "";
+              for (let i = 0; i < amount; i++) {
+                string += `INSERT INTO keys (order_id, g_id, key) VALUES(${
+                  orderDetail.order_id
+                }, ${orderDetail.g_id}, '${generateKey(16)}'); `;
+              }
+              database.transactionQuery(string).catch((err) => {
+                console.log(err);
+                insertKey(amount);
+              });
+            }
+          } else {
+            emailHtml = "<p>Below is your game key</p><ul>";
+            for (let i = 0; i < orderDetail.amount; i++) {
+              emailHtml += `<li> ${orderDetail.g_name} - ${generateKey(16)} </li>`;
+            }
+            emailHtml += "</ul>";
+          }
+        });
+      });
+    let html = `<p>You have been successful make a payment total ${total} on ${paypalRes.create_time}</p>${emailHtml?emailHtml:""}`;
+    sendMail(paypalRes.payer.email_address, "Thank you for purchase", { html });
     cache.del(id);
     res.status(201).json({
       done: "true",
@@ -212,14 +237,4 @@ router.post("/orderDetails", (req, res, next) => {
     });
 });
 
-function generateKey() {
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  var charactersLength = characters.length;
-  let string = '';
-  for ( var i = 0; i <= 16; i++ ) {
-    string += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
-  return string;
-}
 module.exports = router;
